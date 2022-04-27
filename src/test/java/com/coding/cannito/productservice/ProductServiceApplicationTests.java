cb004626a6bb436f76dc5c42badf4a352e187617
Elio Cannito <cannito.elio@gmail.com>
2022-04-27 21:11:19 +0200

    Initial commit

---
+++ src/test/java/com/coding/cannito/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.cannito.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
